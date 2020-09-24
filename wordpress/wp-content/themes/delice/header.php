efeb055dfb7412ece6a33801389d95cd0353d61f
lzervini <lezervinia@gmail.com>
2020-09-24 14:31:19 +0200

    Ajout des pages pour le menu

--- wordpress/wp-content/themes/delice/header.php
+++ wordpress/wp-content/themes/delice/header.php
@@ -12,9 +12,24 @@
 
 <body <?php body_class(); ?>>
 <header class="header">
-    <!-- Lors du clic sur le logo, on atterit obligatoirement sur l'accueil -->
+    <!-- LOGO -->
     <a href="<?php echo home_url( '/' ); ?>">
+    <!-- Lors du clic sur le logo, on atterit obligatoirement sur l'accueil -->
       <img src="<?php echo get_template_directory_uri(); ?>/img/logo_delice.svg" alt="Logo de Les Délices Sucrés">
-    </a>  
+    </a> 
+
+    <!-- MENU -->
+     <div>
+        <?php
+            if ( has_nav_menu('header-menu')) {
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'header-menu',
+                        'menu_class' => 'navbar'
+                    )
+                );
+            }
+        ?>
+    </div>
   </header>
     <?php wp_body_open(); ?>
