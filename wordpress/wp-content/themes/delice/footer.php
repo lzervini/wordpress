a7f80875702fdcf1848068967d5e219ee3787507
lzervini <lezervinia@gmail.com>
2020-09-25 13:28:43 +0200

    Changement de style

--- wordpress/wp-content/themes/delice/footer.php
+++ wordpress/wp-content/themes/delice/footer.php
@@ -1,7 +1,14 @@
     <!-- On récupère les scripts -->
 <?php wp_footer(); ?>
 <footer>
-
+<div class="scallop-up"></div>
+<div class="colour-block">
+ <p class="text-center">Copyright 2020 - ZERVINI Léa</p>
+ <div class="d-flex justify-content-between mx-5">
+ <p> MENTIONS LEGALES</p>
+ <p>Politiques de Confidentialités</p>
+</div>
+</div>
 </footer>
 </body>
 </html>
