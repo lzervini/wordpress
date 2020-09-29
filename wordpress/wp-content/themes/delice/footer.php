af575a445f7233faf7d8625e75c2aa8f8969d670
lzervini <lezervinia@gmail.com>
2020-09-29 10:29:14 +0200

    Ajout de la politique de confidentialité

--- wordpress/wp-content/themes/delice/footer.php
+++ wordpress/wp-content/themes/delice/footer.php
@@ -6,8 +6,7 @@
  <p class="text-center">Copyright 2020 - ZERVINI Léa</p>
  <div class="d-flex justify-content-between mx-5">
  <p> MENTIONS LEGALES</p>
- <p>Politiques de Confidentialités</p>
-</div>
+ <p><a href="<?php echo get_permalink(get_page_by_title('Politique de confidentialité')); ?>">Politique de confidentialité</a></p></div>
 </div>
 </footer>
 </body>
---
+++ wordpress/wp-content/themes/delice/politiques.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  Template Name: Politique de confidentialité
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les délices Sucrés</title>
+</head>
+<body>
+    <?php get_header(); ?>
+
+    <div><?php the_content(); ?></div>
+
+
+
+    <?php get_footer(); ?>
+</body>
+</html>
