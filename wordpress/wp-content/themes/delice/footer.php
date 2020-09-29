69114da73a86f754a207a53f097d26198159b16d
lzervini <lezervinia@gmail.com>
2020-09-29 11:05:15 +0200

    Suppression des classes du séparateur

--- wordpress/wp-content/themes/delice/footer.php
+++ wordpress/wp-content/themes/delice/footer.php
@@ -1,13 +1,12 @@
     <!-- On récupère les scripts -->
 <?php wp_footer(); ?>
-<footer>
-<!-- <div class="scallop-up"></div> -->
-<div class="colour-block">
- <p class="text-center">Copyright 2020 - ZERVINI Léa</p>
- <div class="d-flex justify-content-between mx-5">
- <p> MENTIONS LEGALES</p>
- <p><a href="<?php echo get_permalink(get_page_by_title('Politique de confidentialité')); ?>">Politique de confidentialité</a></p></div>
-</div>
-</footer>
+    <footer>
+        <div class="colour-block">
+            <p class="text-center">Copyright 2020 - ZERVINI Léa</p>
+        <div class="d-flex justify-content-between mx-5">
+            <p> MENTIONS LEGALES</p>
+            <p><a href="<?php echo get_permalink(get_page_by_title('Politique de confidentialité')); ?>">Politique de confidentialité</a></p></div>
+        </div>
+    </footer>
 </body>
 </html>
